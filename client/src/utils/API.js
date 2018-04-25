d262dcdf7aeac6bc7e7ae40b5ac836a26aa2f2cf
Quintessa Anderson <anderson.quintessa@gmail.com>
2018-04-25 13:44:57 -0500

    modified utils/API.js, scripts files, api routes, and pushing code for controller.js

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,36 +1,35 @@
 import axios from "axios";
 
-// Export an object containing methods we'll use for accessing the Dog.Ceo API
+// Export an object containing methods we'll use for accessing the NYT API
+
 
 export default {
-  getArticles: function() {
-    return axios.get("https://dog.ceo/api/breeds/image/random");
-  }
+    // Gets all articles
+    getArticles: function() {
+        return axios.get("/api/articles");
+    },
 
-};
+    // Save articles
+    getArticles: function() {
+        return axios.get("/api/articles");
+    },
 
-//   getDogsOfBreed: function(breed) {
-//     return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-//   },
-//   getBaseBreedsList: function() {
-//     return axios.get("https://dog.ceo/api/breeds/list");
-//   }
+    // Deletes the book with the given id
+    deleteBook: function(id) {
+        return axios.delete("/api/articles/:id" + id);
+    },
 
-// export default {
-//   // Gets all articles
-//   getarticles: function() {
-//     return axios.get("/api/articles");
-//   },
-//   // Gets the book with the given id
+    getArticles: function() {
+        return axios.get("/*");
+    }
+
+    };
+
+  //   // Gets the book with the given id
 //   getBook: function(id) {
 //     return axios.get("/api/articles/" + id);
-//   },
-//   // Deletes the book with the given id
-//   deleteBook: function(id) {
-//     return axios.delete("/api/articles/" + id);
 //   },
 //   // Saves a book to the database
 //   saveBook: function(bookData) {
 //     return axios.post("/api/articles", bookData);
 //   }
-// };
--- routes/api/articles.js
+++ routes/api/articles.js
@@ -2,30 +2,30 @@ const router = new express.Router();
 const articlesController = require("../../controllers/articlesController");
 
 // Routing
-
 // Define any API routes first
 
-// Get saved articles
-router.get("/api/articles", articlesController.find);
-// Save articles
-router.post("/api/articles", articlesController.insert);
-// delete saved articles
-router.delete("/api/articles/delete/:id", articlesController.delete);
+router
+.route("/")
+.get(articlesController.findAll)
+
+// Matches with "/api/article/:id"
+router
+  .route("/api/articles/")
+  // Get saved articles
+  .get(articlesController.findById)
+  // Save articles
+  .put(articlesController.update)
+
+router
+  .route("/api/articles/:id")
+  // Delete an Article
+  .delete(articlesController.remove);
+
 // Send every other request to the React app
 router.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-// // Matches with "/api/article"
-// router.route("/")
-//   .get(articleController.findAll)
-//   .post(articleController.create);
-
-// // Matches with "/api/article/:id"
-// router
-//   .route("/:id")
-//   .get(articleController.findById)
-//   .put(articleController.update)
-//   .delete(articleController.remove);
 
 module.exports = router;
+
